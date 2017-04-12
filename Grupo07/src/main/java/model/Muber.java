029ab2d3e5f2ca054d7bdbaf807dd5ca15a8f104
GIANNI ORLANDO BALLERINI <gianniballerini@gmail.com>
2017-04-12 18:30:21 -0300

    me olvide de hacer ctrl-s en eclipse antes de comitear

--- Grupo07/src/main/java/model/Muber.java
+++ Grupo07/src/main/java/model/Muber.java
@@ -1,24 +1,16 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
+import java.util.Iterator;
 
 public class Muber {
 	
 	private Collection<Driver> drivers;
 	private Collection<Passenger> passengers;
-	private Date licenceDate;
 	
 	public Muber(){
 		
-	}
-
-	public Date getLicenceDate() {
-		return licenceDate;
-	}
-
-	public void setLicenceDate(Date licenceDate) {
-		this.licenceDate = licenceDate;
 	}
 
 	public Collection<Passenger> getPassengers() {
@@ -35,6 +27,22 @@ public class Muber {
 
 	public void setDrivers(Collection<Driver> drivers) {
 		this.drivers = drivers;
+	}
+	
+	public Collection<Trip> getOpenTrips(){
+		Collection<Trip> openTrips = new ArrayList<Trip>();
+		if(drivers.size() > 0){
+			for (Iterator<Driver> iterator = drivers.iterator(); iterator.hasNext();) {
+				Collection<Trip> trips = iterator.next().getTrips();
+				for(Iterator<Trip> iterator2 = trips.iterator(); iterator2.hasNext(); ){
+					Trip trip = iterator2.next();
+					if(trip.getState() == true){
+						openTrips.add(trip);
+					}
+				}
+	        }
+		}
+		return openTrips;
 	}
 	
 }
