e9f4502c338c6bdf0e839a54b7c486659d4f856b
Camila Bignone <camilabignone@gmail.com>
2017-05-06 15:49:24 -0300

    add /viajes/nuevos

--- MuberRESTful/src/main/java/bd2/web/MuberRestController.java
+++ MuberRESTful/src/main/java/bd2/web/MuberRestController.java
@@ -1,5 +1,6 @@
 package bd2.web;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -141,9 +142,9 @@ public class MuberRestController {
 		return new Gson().toJson(aMap);		
 	}
 	
-	//Obtener la información de un conductor
-	//(nombre de usuario, viajes realizados, puntaje promedio y fecha de licencia)
-	//TODO:Crear bien la url
+	//Obtener la información de un conductor (nombre de usuario, viajes realizados, puntaje promedio y fecha de licencia)
+	//TODO:Crear bien la url, tiene que ser con "/detalles?conductorId=1" creo... preguntarlo
+	//TODO:Cuando creemos bien el json de los viajes, usarlo aca
 	@RequestMapping(value = "/conductores/detalles/{conductorId}",  method = RequestMethod.GET, produces = "application/json", headers = "Accept=application/json")
 	public String conductoresDetalles(@PathVariable(value="conductorId") Long conductorId) {
 
@@ -151,11 +152,11 @@ public class MuberRestController {
 		List<Driver> driverList = getDriver(conductorId);
 		for (Driver d : driverList){ 
 			aMap.put("Nombre", d.getFullName());
-			//Itera por cada viaje para listar los viajer realizadob
+			//Itera por cada viaje para listar los viajes realizado
 			for (Trip t : d.getTrips()){ 
 				aMap.put(t.getIdTrip().toString(), t.getDate());
 			}
-			aMap.put("puntaje promedio", d.averageScore());
+			aMap.put("Puntaje promedio", d.averageScore());
 			aMap.put("Fecha de licencia", d.getLicenseDate());
 			
 		}
@@ -164,6 +165,47 @@ public class MuberRestController {
 	
 	
 	//Crear un viaje
+	//Este servicio recibe los siguientes parámetros: origen, destino, conductorId,	costoTotal, cantidadPasajeros
+	@RequestMapping(value = "/viajes/nuevo", 
+					method = RequestMethod.POST, 
+					produces = "application/json", 
+					headers = "Accept=application/json")
+	
+	public String viajesNuevo(
+			@RequestParam("origen") String origin,
+			@RequestParam("destino") String destination,
+			@RequestParam("conductorId") Long idDriver,
+			@RequestParam("costoTotal") double price,
+			@RequestParam("cantidadPasajeros") Integer maxPassenger
+			) {
+		
+		Map<String, Object> aMap = new HashMap<String, Object>();	
+		
+		//Verifico lo que me llega por parametro
+		aMap.put("Origen", origin );
+		aMap.put("Destino", destination );
+		aMap.put("conductorId", idDriver );
+		aMap.put("costoTotal", price );
+		aMap.put("cantidadPasajeros", maxPassenger );
+		
+		
+		Date date = new Date();
+		//uso el metodo de /conductores. 
+		//TODO: Arreglgar, Si el driver con ese id no existe, todo se rompe porque lo de abajo da vacio y se intenta crear un trip sin conductor y Error!!!!!!!!!!!!
+		Driver driver = getDriver(idDriver).get(0);
+		Trip aTrip = new Trip(driver, date, maxPassenger, price, origin, destination);
+		//Imprimo giladas, 
+		aMap.put("v Origen", aTrip.getOrigin());
+		aMap.put("v Destino", aTrip.getDestination());
+		aMap.put("v conductorId", aTrip.getDriver().getFullName());
+		aMap.put("v costoTotal", aTrip.getPrice());
+		aMap.put("v cantidadPasajeros", aTrip.getMaxPassenger());
+		aMap.put("v date", aTrip.getDate());
+		return new Gson().toJson(aMap);	
+		//TODO:Crear un viaje implica guardarlo, no?, hacer un session.save en algun lugar. no aca :)
+	}
+	// curl -d "costoTotal=10&cantidadPasajeros=5&origen=pepe&destino=sarasa&conductorId=1" http://localhost:8080/MuberRESTful/rest/services/viajes/nuevo
+
 	
 	
 	
