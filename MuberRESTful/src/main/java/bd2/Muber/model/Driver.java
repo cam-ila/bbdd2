657e70e5cae3e3e6906e935f86eb78ec66641df0
GIANNI ORLANDO BALLERINI <gianniballerini@gmail.com>
2017-06-08 13:47:28 -0300

    commenting

--- MuberRESTful/src/main/java/bd2/Muber/model/Driver.java
+++ MuberRESTful/src/main/java/bd2/Muber/model/Driver.java
@@ -6,14 +6,26 @@ import java.util.Iterator;
 
 import bd2.Muber.model.Trip;
 import bd2.Muber.model.User;
+/**
+ * Driver is a subclass from User that 
+ * implements its own methods.
+ */
 
 public class Driver extends User {
+	/**
+    * A collection of trips owned
+    */
 	private Collection<Trip> trips;
+	/**
+    * Date variable with the driver license's date
+    */
 	private Date licenseDate;
 	
 	public Driver() {
 	}
-	
+	/**
+    * Driver object's constructor
+    */
 	public Driver(String fullName, String password, Date licenseDate){
 		super(fullName, password);
 		this.licenseDate = licenseDate;
@@ -36,14 +48,29 @@ public class Driver extends User {
 	public void setLicenseDate(Date date) {
 		this.licenseDate = date;
 	}
+	/**
+    * Creates a new trip and adds it to its collection of trips
+    */
 	public void createAndAddTrip(String origin, String destination, Date date, Integer maxPassenger, double price){
 		Trip trip = new Trip(this, date, maxPassenger, price, origin, destination);
 		this.trips.add(trip);
 	}
-	public void endTrip(Trip trip){
+	/**
+	 * Closes a trip.
+    * @return true  when the trip closed succesfully
+    * @return false when the trip was already closed
+    */
+	public boolean endTrip(Trip trip){
 		trip.close();
-//		false significa que el viaje esta cerrado
 	}
+	/**
+    * Calculates the average driver's score.
+    * Iterates through all of the driver's trips and adds the average score
+    * of each trip to the parameter average.
+    * 
+    * @param average  the average score of the trips iterated
+    * @return		  the full adding of the trip's average scores.
+    */
 	public float averageScore(){
 		float average = 0;
 		if(trips.size() > 0){
@@ -54,6 +81,12 @@ public class Driver extends User {
 		}
 		return average;
 	}
+	/**
+    * Checks of an open trip. If there is returns true, else returns false.
+    * 
+    * @return true if there is an open trip
+    * @return false if there is not an open trip
+    */
 	public boolean haveOpenTrip(){
 		for (Trip t: this.trips){
 			if (t.getState()){
